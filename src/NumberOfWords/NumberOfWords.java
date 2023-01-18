6b06a7c9ec3174ff5debb5f567419686f61b9a85
brownsally2 <heej0258@naver.com>
2023-01-18 23:59:29 +0900

    1/18

---
+++ src/NumberOfWords/NumberOfWords.java
@@ -0,0 +1,17 @@
+package NumberOfWords;
+
+import java.util.Scanner;
+
+public class NumberOfWords {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        str = str.trim();
+        String[] numOfWords = str.split(" ");
+        if(numOfWords[0].equals("")){
+            System.out.println(0);
+        }else {
+            System.out.println(numOfWords.length);
+        }
+    }
+}
