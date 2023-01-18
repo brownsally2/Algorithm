8629337b49a4ea307aa3802c955ba56c76f7b150
brownsally2 <heej0258@naver.com>
2023-01-18 22:46:29 +0900

    1/18

---
+++ src/Subtraction/Subtraction.java
@@ -0,0 +1,12 @@
+package Subtraction;
+
+import java.util.Scanner;
+
+public class Subtraction {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        System.out.println(A-B);
+    }
+}
