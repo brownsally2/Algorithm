d352d5c39730430741753d1fc8e20d76a7d31db4
brownsally2 <heej0258@naver.com>
2023-01-25 21:05:57 +0900

    1/25

--- src/Star/Star.java
+++ src/Star/Star.java
@@ -2,13 +2,16 @@ package Star;
 
 import java.util.Scanner;
 
-public class Star {
+public class Star{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
+        // 줄에 해당
         for(int i = 1; i <= N; i++){
-            for(int j = 5; j > i; j--){
+            //공백 입력
+            for(int j = N; j > i; j--){
                 System.out.print(" ");
+                // 별 찍는 갯수
             }for(int j = 0; j < i ; j++){
                 System.out.print("*");
             }
