831ece5a1edd76dc0bfa0de5061a0cc0938e7080
brownsally2 <heej0258@naver.com>
2023-01-26 15:53:12 +0900

    1/26

---
+++ src/Maximum/Maximum.java
@@ -0,0 +1,23 @@
+package Maximum;
+
+import java.util.Scanner;
+
+public class Maximum {
+    public static void main(String[] args) {
+       int[] arr = new int[10];
+       Scanner sc = new Scanner(System.in);
+       arr[0] = 0;
+       for(int i = 1; i < 10; i++){
+           arr[i] = sc.nextInt();
+           if(arr[0] < arr[i]){
+               arr[0] = arr[i];
+           }
+       }
+       System.out.println(arr[0]);
+       for(int i = 1; i< 10; i++){
+           if(arr[0] == arr[i]){
+               System.out.println(i);
+           }
+       }
+    }
+}
