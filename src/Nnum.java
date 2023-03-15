a26892eaa62111f8d427a1e5cae3a1a4bc80a3eb
brownsally2 <heej0258@naver.com>
2023-03-15 11:44:01 +0900

    3/15

---
+++ src/Nnum.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Nnum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int cnt = A;
+        for(int i = 1; i <= cnt; i++){
+            System.out.println(A);
+            A--;
+        }
+    }
+}
