aa29cc10f406701010fe4c9b37452f68cca6a179
brownsally2 <heej0258@naver.com>
2023-03-17 16:53:39 +0900

    3/17

--- src/Constant/Constant2.java
+++ src/Constant/Constant2.java
@@ -7,7 +7,7 @@ public class Constant2 {
         Scanner sc  = new Scanner(System.in);
         int A = sc.nextInt();
         int B = sc.nextInt();
-
+        // StringBuilder의 reverse()를 사용하기 위함
         int C = Integer.parseInt(new StringBuilder().append(A).reverse().toString());
         int D = Integer.parseInt(new StringBuilder().append(B).reverse().toString());
         // 큰 값을 리턴
